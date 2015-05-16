c718f718135137a271c62bc1117f936f9f6af519
andrew <smirnov.andrew@mail.ru>
2015-05-16 13:33:35 +0300

    элементы для редактирования вынесены в скоп

--- app/modules/employee-schedule/directives/calendar-day/calendar-day.directive.js
+++ app/modules/employee-schedule/directives/calendar-day/calendar-day.directive.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('rbt.employee-schedule').directive('calendarDay', /*@ngInject*/function () {
+angular.module('rbt.employee-schedule').directive('calendarDay', /*@ngInject*/function (DAY_TYPES) {
   return {
     restrict: 'E',
     templateUrl: "assets/view/employee-schedule/directives/calendar-day/calendar-day.html",
@@ -16,9 +16,32 @@ angular.module('rbt.employee-schedule').directive('calendarDay', /*@ngInject*/fu
       };
 
       scope.$watch("isSelectedDay", function (newValue) {
-        if(!newValue)
+        if (!newValue)
           scope.isEditDay = false;
       });
+
+      scope.editDayTypes = [
+        {
+          dayType: DAY_TYPES.GREEN,
+          caption: "Рабочий день",
+          circle_class: "green-circle"
+        },
+        {
+          dayType: DAY_TYPES.WHITE,
+          circle_class: "white-circle",
+          caption: "Не рабочий день"
+        },
+        {
+          dayType: DAY_TYPES.ORANGE,
+          circle_class: "orange-circle",
+          caption: "Пропуск"
+        },
+        {
+          dayType: DAY_TYPES.ORANGE,
+          circle_class: "red-circle",
+          caption: "Прогул"
+        }
+      ];
     }
   };
 });
